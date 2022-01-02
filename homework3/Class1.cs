181dc856a5dd28a46b482935f9fd7adf4fc0b3a5
s9904320 <91059581+s9904320@users.noreply.github.com>
2022-01-02 21:44:03 +0800

    Add project files.

---
+++ homework3/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace homework3
+{
+    public class Class1
+    {
+        public static string bandNameGenerator(string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                str = "is null";
+            }
+            else
+            {
+                string firstLetter = str.Substring(0, 1);
+                string endLetter = str.Substring(str.Length-1, 1);
+
+                if (firstLetter != endLetter)
+                {
+                    firstLetter = firstLetter.ToUpper();
+                    str = "The " + firstLetter + str.Substring(1);
+                }
+                else
+                {
+                    firstLetter = firstLetter.ToUpper();
+                    str = firstLetter + str.Substring(1) + str.Substring(1);
+                }
+            }
+
+            return str;
+        }
+    }
+}
---
+++ homework3TestsBandNameGenerator/Class1TestsBandNameGenerator.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using homework3;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework3.TestsBandNameGenerator
+{
+    [TestClass()]
+    public class Class1TestsBandNameGenerator
+    {
+
+        [TestMethod()]
+        public void bandNameGeneratorTestBandNameGenerator()
+        {
+            Assert.AreEqual("is null", Class1.bandNameGenerator(""));
+            Assert.AreEqual("is null", Class1.bandNameGenerator(null));
+            Assert.AreEqual("The Knife", Class1.bandNameGenerator("knife"));
+            Assert.AreEqual("Tartart", Class1.bandNameGenerator("tart"));
+            Assert.AreEqual("Sandlesandles", Class1.bandNameGenerator("sandles"));
+            Assert.AreEqual("The Bed", Class1.bandNameGenerator("bed"));
+        }
+    }
+}
